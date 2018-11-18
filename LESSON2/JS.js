c4b5ede5dd09cbb48e43dd6544ad631ec70b195c
Dmitry <dontbeatmybrin@gmail.com>
2018-11-18 13:39:38 +0300

    Подготовка к домашнему заданию

--- LESSON2/JS.js
+++ LESSON2/JS.js
@@ -1,19 +1,37 @@
 let money = prompt('Ваш бюджет на месяц?', 0);
 let time = prompt('Введите дату в формате YYYY-MM-DD', 0);
-let a = prompt('Введите обязательную статью расходов в этом месяце');
-let b = prompt('Во сколько обойдется?');
-let c = prompt('Введите обязательную статью расходов в этом месяце');
-let d = prompt('Во сколько обойдется?');
+
 let appData = {
     budjet: money,
     date: time,
     expenses: {},
-    optionalExpenses: {
-
-    },
+    optionalExpenses: {},
     income: [],
     savings: false
 }
-appData.expenses.a = b;
-appData.expenses.c = d;
-alert("Ваш бюджет на сегодня: " + money/30);
+for(let i = 0; i<2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце', '');
+    let b = prompt('Во сколько обойдется?', '');
+    
+    if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50 ){
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
+
+    }
+};  
+
+
+appData.moneyPerDay = appData.budjet/30;
+
+alert("Ваш бюджет на сегодня: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100){
+    console.log('Минимум');
+} else if (appData.moneyPerDay > 100 &&  appData.moneyPerDay < 2000){
+    console.log('Нормально');
+} else if (appData.moneyPerDay < 1000) {
+    console.log('Богато');
+} else {
+    console.log('Ошибка');
+}
