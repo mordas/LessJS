d3668ece2ba3f8daebfd02f4533b9d0ddd8b73ae
Dmitry <dontbeatmybrin@gmail.com>
2018-11-18 14:01:24 +0300

    Добавил еще два варианта циклов, через While и через DO WHILE

--- LESSON2/JS.js
+++ LESSON2/JS.js
@@ -20,6 +20,29 @@ for(let i = 0; i<2; i++) {
 
     }
 };  
+////////1 Решение, через WHILE
+// let i = 0;
+// while(i<2) {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', '');
+//     let b = prompt('Во сколько обойдется?', '');
+    
+//     if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50 ){
+//         console.log('done');
+//         appData.expenses[a] = b;
+//         i++;
+// }}
+
+////// 2 Решение через DO WHILE
+// let i = 0;
+// do {
+//         let a = prompt('Введите обязательную статью расходов в этом месяце', '');
+//         let b = prompt('Во сколько обойдется?', '');
+        
+//         if ( (typeof(a))=== 'string' && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50 ){
+//             console.log('done');
+//             appData.expenses[a] = b;
+//             i++;
+//     }} while (i<2);
 
 
 appData.moneyPerDay = appData.budjet/30;
