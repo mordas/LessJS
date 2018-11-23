27069929c69c0765d51fa1187f74c83a33d971fa
Dmitry <dontbeatmybrin@gmail.com>
2018-11-23 06:46:37 +0300

    завершил урок

---
+++ budget-app/src/js/main.js
@@ -0,0 +1,21 @@
+let begin = document.getElementById('start');
+let val = document.getElementsByClassName('budget-value daybudget-value level-value expenses-value optionalexpenses-value income-value monthsavings-value yearsavings-value yearsavings-value month-value day-value');
+let inp = document.getElementsByClassName('expenses-item');
+let button = document.getElementsByTagName('button');
+let conf = button[0, 1];
+let count = button[2];
+let optionalexpenses = document.querySelectorAll('.optionalexpenses');
+let chooseincome = document.querySelector('.choose_income');
+let checksavings = document.querySelector('.checksavings');
+let choosesum = document.querySelector('.choose_sum');
+let year = document.querySelector('.year');
+let month = document.querySelector('.month');
+let day = document.querySelector('.day');
+
+// console.log(begin);
+// console.log(val);     
+// console.log(inp);
+// console.log(button);
+// console.log(conf);
+// console.log(optionalexpenses);
+
