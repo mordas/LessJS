0e65ae9bba5643dbb7530e405bc63323278b20e4
dmitryMordas <dontbeatmybrain@gmail.com>
2018-11-29 06:56:08 +0300

    урок с es6

---
+++ yoga/js/class.js
@@ -0,0 +1,12 @@
+class Options {
+    constructor(height, width, bg, fontSize, textAlign) {
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.fontSize = fontSize;
+        this.textAlign = textAlign;
+    }
+    met() {
+        
+    }
+}
--- yoga/js/script.js
+++ yoga/js/script.js
@@ -5,16 +5,23 @@ window.addEventListener('DOMContentLoaded', function() {
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent(a) {
+    // function hideTabContent(a) {
+    //     for (let i = a; i < tabContent.length; i++) {
+    //         tabContent[i].classList.remove('show');
+    //         tabContent[i].classList.add('hide');
+    //     }
+    // }
+
+    let hideTabContent = (a) => {
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
-        }
+        } 
     }
 
     hideTabContent(1);
 
-    function showTabContent(b) {
+    let showTabContent = (b) => {
         if (tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
@@ -39,7 +46,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     let deadline = '2018-11-21';
 
-    function getTimeRemaining(endtime) {
+    let getTimeRemaining = (endtime) => {
         let t = Date.parse(endtime) - Date.parse(new Date()),
         seconds = Math.floor((t/1000) % 60),
         minutes = Math.floor((t/1000/60) % 60),
@@ -53,14 +60,14 @@ window.addEventListener('DOMContentLoaded', function() {
         };
     }
 
-    function setClock(id, endtime) {
+    let setClock = (id, endtime) => {
         let timer = document.getElementById(id),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
             timeInterval = setInterval(updateClock, 1000);
             
-        function updateClock() {
+        let updateClock = () => {
             let t = getTimeRemaining(endtime);
 
             function addZero(num){
